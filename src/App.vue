a6b2e3ac4dcd32cce39882902150b2c347051165
PatrickMaul <97639335+PatrickMaul@users.noreply.github.com>
2022-09-17 16:48:58 +0200

    INIT

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.app
+  main
+    router-view
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    test(x) {
+      return x + 1;
+    },
+  },
+};
+</script>
