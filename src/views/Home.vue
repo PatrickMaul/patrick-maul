bbb23864d0383e6760093e5a2d2eea0ecfcaeeb1
Patrick Maul <maulpatrick1996@gmail.com>
2022-10-11 16:17:00 +0200

    feat: add hero, only placeholder with size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,31 @@
 <template lang="pug">
 #home
-  p HOME
+  .hero
+    span i'm a hero
 </template>
 
 <script>
 export default {
   name: "Home",
+  mounted() {
+    const applicationBarHeight = document
+      .getElementById("application-bar")
+      .getBoundingClientRect().height;
+    console.log(applicationBarHeight);
+
+    const hero = document.getElementById("home");
+    let homeContainerPadding = 16;
+
+    hero.querySelector(".hero").style.height =
+      window.innerHeight - applicationBarHeight - homeContainerPadding + "px";
+  },
 };
 </script>
+
+<style lang="sass" scoped>
+.hero
+  display: flex
+
+  span
+    margin: auto
+</style>
